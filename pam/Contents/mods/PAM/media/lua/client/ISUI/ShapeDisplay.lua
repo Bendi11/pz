5dd251a621cc8564fd5d26c393c4f6cac87a72fd
Bendi11 <bkliebmann@gmail.com>
2023-04-20 11:44:00 -0700

    Added ShapeDisplay ISUIElement to display sheet stock

--- pam/Contents/mods/PAM/media/lua/client/ISUI/ShapeDisplay.lua
+++ pam/Contents/mods/PAM/media/lua/client/ISUI/ShapeDisplay.lua
@@ -1,9 +1,11 @@
 require 'ISUI/ISUIElement'
 
 local Shape = require 'lib/geom/Shape'
+local geom = require 'lib/geom'
 
 ---@class ShapeDisplay: ISUIElement
 ---@field shape Shape
+---@field pxPerIn number
 local ShapeDisplay = ISUIElement:derive('ShapeDisplay')
 
 ---Create a new display area for shapes
@@ -11,16 +13,34 @@ local ShapeDisplay = ISUIElement:derive('ShapeDisplay')
 ---@param y integer
 ---@param width integer
 ---@param height integer
+---@param shape Shape
+---@param aabb AABB|nil
 ---@return ShapeDisplay
-function ShapeDisplay:new(x, y, width, height)
+function ShapeDisplay:new(x, y, width, height, shape, aabb)
     local o = ISUIElement.new(self, x, y, width, height)
+
+    o.shape = shape
+
+    local size = math.sqrt(self.width^2 + self.height^2)
+
+    aabb = aabb or shape:aabb()
+
+    o.pxPerIn = size / geom.distance(aabb[1], aabb[2])
+
 
     return o
 end
 
----Initialise state for this shape display, calculating the maximum extent of the shape if it is not given
----@param shape Shape
----@param extends AABB
-function ShapeDisplay:initialise(shape)
-    self.shape = shape
+function ShapeDisplay:render()
+    local w = self.width / 2
+    local h = self.height / 2
+    for line in self.shape:lines() do
+        self:drawLine2(
+            line[1][1] * self.pxPerIn + w, line[1][2] * self.pxPerIn + h,
+            line[2][1] * self.pxPerIn + w, line[2][2] * self.pxPerIn + h,
+            1, 1, 1, 1
+        )
+    end
+
+    ISUIElement.render(self)
 end
