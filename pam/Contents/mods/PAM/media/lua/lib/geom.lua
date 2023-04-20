3f847d4b6f9a5b8fed8181c70bfc7cd3276c57bc
Bendi11 <bkliebmann@gmail.com>
2023-04-20 10:25:00 -0700

    Added shape division logic

--- pam/Contents/mods/PAM/media/lua/lib/geom.lua
+++ pam/Contents/mods/PAM/media/lua/lib/geom.lua
@@ -1,4 +1,22 @@
+local geom = {}
 
-return {
-    Shape = require 'lib.geom.Shape',
-}
+local sqrt = math.sqrt
+
+---Calculate the distance from `p1` to `p2`
+---@param p1 Point
+---@param p2 Point
+---@return number
+function geom.distance(p1, p2)
+    return sqrt( (p2[1] - p1[1])^2 + (p2[2] - p1[2])^2)
+end
+
+---Check if the given point is on the given line segment
+---@param line Line
+---@param point Point
+---@return boolean
+function geom.point_on_line(line, point)
+    local dist = (geom.distance(point, line[1]) + geom.distance(point, line[2]))
+    return dist == geom.distance(line[1], line[2])
+end
+
+return geom
--- pam/Contents/mods/PAM/media/lua/lib/geom/Shape.lua
+++ pam/Contents/mods/PAM/media/lua/lib/geom/Shape.lua
@@ -1,3 +1,7 @@
+local geom = require 'lib/geom'
+
+local json = require 'lib.json'
+
 ---@alias LinesIterator fun(): Line|nil
 ---@alias Point { [1]: number, [2]: number }
 ---@alias Line { [1]: Point, [2]: Point }
@@ -9,11 +13,9 @@ local Shape = {}
 ---@return Shape
 function Shape:new(list)
     ---@type Shape
-    local o = {}
+    local o = list
     self.__index = self
     setmetatable(o, self)
-
-    o = list
 
     return o
 end
@@ -36,7 +38,10 @@ end
 ---Get the point in the list after this point
 ---@param idx number
 ---@return number
-function Shape:next_point(idx) return (idx + 1) % #self end
+function Shape:next_point(idx)
+    if (idx + 1) > #self then return 1 else return idx + 1 end
+end
+function Shape:prev_point(idx) if idx == 1 then return #self else return idx - 1 end end
 
 ---Get an iterator over all lines that make up the perimeter of this shape
 ---@return LinesIterator
@@ -44,8 +49,15 @@ function Shape:lines()
     local i = 0
     return function()
         i = i + 1
-        if i <= #self then return {self[i], self:next_point(i)} end
+        if i <= #self then return {self[i], self[self:next_point(i)]} end
     end
+end
+
+---Get a line array by first point index
+---@param idx integer
+---@return Line|nil
+function Shape:line_by_idx(idx)
+    if idx <= #self then return { self[idx], self:next_point(idx) } end
 end
 
 ---Get the total area of this shape
@@ -59,5 +71,72 @@ function Shape:area()
 
     return math.abs(area) / 2
 end
+
+---Get the index of a given point
+---@param point Point
+---@return integer
+function Shape:point_idx(point)
+    for i=1, #self do
+        if self[i] == point then return i end
+    end
+end
+
+---Divide the shape along a given `cut` line
+---@param cut Shape
+---@return Shape, Shape|nil
+function Shape:divide(cut)
+    local point_on_line, insert = geom.point_on_line, table.insert
+
+    local first_divider, second_divider
+
+    for line in self:lines() do
+        if point_on_line(line, cut[1]) then
+            first_divider = line
+        end
+        if point_on_line(line, cut[#cut]) then
+            second_divider = line
+        end
+    end
+
+    if not first_divider or not second_divider then return self, nil end
+
+    local function divided(first, next)
+        local idx = self:point_idx(first_divider[first])
+        local i = idx
+        local point
+
+        local part = {}
+        while true do
+            point = self[i]
+            if (second_divider ~= first_divider and point == second_divider[1]) or point == second_divider[2] then
+                break
+            end
+            insert(part, point)
+
+            i = next(self, i)
+            if i == idx then return self, nil end
+        end
+
+        for j = #cut,1,-1 do
+            insert(part, cut[j])
+        end
+
+        return part
+
+    end
+
+    return divided(1, self.prev_point), divided(2, self.next_point)
+end
+
+local square = Shape.new_square(1)
+local cut = Shape:new {
+    {-0.5, 0},
+    {0, 0},
+    {0, 0.25},
+    {-0.5, 0.25}
+}
+local p1, p2 = square:divide(cut)
+
+print(json.encode(p1) .. '\n\n' .. json.encode(p2))
 
 return Shape
